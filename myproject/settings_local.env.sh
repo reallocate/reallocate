8723139352f2e8938ed140f804bc734ad23c0c17
David Dehghan <ddehghan@gmail.com>
2013-03-16 11:12:35 -0700

    setting files

---
+++ myproject/settings_local.env.sh
@@ -0,0 +1,20 @@
+#
+#  _____                      _      _____                          _ _
+# |  __ \                    | |    / ____|                        (_) |
+# | |  | | ___    _ __   ___ | |_  | |     ___  _ __ ___  _ __ ___  _| |_
+# | |  | |/ _ \  | '_ \ / _ \| __| | |    / _ \| '_ ` _ \| '_ ` _ \| | __|
+# | |__| | (_) | | | | | (_) | |_  | |___| (_) | | | | | | | | | | | | |_
+# |_____/ \___/  |_| |_|\___/ \__|  \_____\___/|_| |_| |_|_| |_| |_|_|\__|
+#
+#
+
+# Local settings
+
+export S3_KEY=xxxxxxxxxx
+export S3_SECRET=xxxxxxxxxx
+export FACEBOOK_APP_ID=xxxxxxxxxx
+export FACEBOOK_API_SECRET=xxxxxxxxxx
+export GOOGLE_OAUTH2_CLIENT_ID=xxxxxxxxxx
+export GOOGLE_OAUTH2_CLIENT_SECRET=xxxxxxxxxx
+export DEPLOY_ENV=dev
+
