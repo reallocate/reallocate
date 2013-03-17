b6983111e75845dc033483813ef592776d63d882
John Clarke Mills <johnclarkemills@mac.com>
2013-03-17 12:20:14 -0700

    adding about page

--- myproject/urls.py
+++ myproject/urls.py
@@ -11,12 +11,12 @@ urlpatterns = patterns('',
                        # url(r'^$', 'myproject.views.home', name='home'),
                        url(r'', include('social_auth.urls')),
                        url(r'^$', 'website.views.homepage', name='homepage'),
+                       url(r'^about$', 'website.views.about', name='about'),
                        url(r'^signup', 'website.views.signup'),
                        url(r'^logout', 'django.contrib.auth.views.logout', {'next_page': '/'}),
 
                        url(r'^add_project', 'website.views.add_project', name='add_project'),
-                       url(r'^project/(?P<oid>\d+)/add_opportunity', 'website.views.add_opportunity',
-                           name='add_opportunity'),
+                       url(r'^project/(?P<oid>\d+)/add_opportunity', 'website.views.add_opportunity', name='add_opportunity'),
                        url(r'^test_project', 'website.views.test_project', name='test_project'),
                        url(r'^project/(?P<pid>\d+)', 'website.views.view_project', name='view_project'),
 
@@ -26,13 +26,9 @@ urlpatterns = patterns('',
                        url(r'^ajax/follow_project', 'website.ajaxviews.follow_project', name='follow_project'),
                        url(r'^login', 'website.views.login_user', name='login_user'),
 
-                       url(r'^private$', 'website.views.private', name='private'),
-                       # url(r'^test$', 'website.views.test', name='test'),
-
                        # url("^index", TemplateView.as_view(template_name='index.html'), name="mission"),
 
                        # Admin site
-                       #url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
                        url(r'^admin/', include(admin.site.urls)),
 
                        # Server Static Files from Django
