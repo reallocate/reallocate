8343bb7f41e1f2a6b478cce1c158b1826e386dac
Daniel Goodwin <dgoodwin208@gmail.com>
2013-03-17 14:57:13 -0700

    Added in user profile thumbs for the activity feed

--- website/base.py
+++ website/base.py
@@ -5,6 +5,7 @@ def get_current_userprofile(request):
     try:
         return UserProfile.objects.get(user=request.user)
     except Exception, err:
+        raise err
         if request.is_ajax():
             return HttpResponse(json.dumps({'failure': 'no user'}), status=500)
         return HttpResponse("error getting user profile")
