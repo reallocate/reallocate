edf9fb6dad35124a8fdda4740ae0dab7b06b910a
David Dehghan <ddehghan@gmail.com>
2013-03-16 18:27:16 -0700

    added user profile creation support

--- website/admin.py
+++ website/admin.py
@@ -1,7 +1,8 @@
-from models import Project, Opportunity
+from models import Project, Opportunity, UserProfile
 from django.contrib import admin
 
 
 # admin.site.register(LandingPage)
 admin.site.register(Project)
+admin.site.register(UserProfile)
 admin.site.register(Opportunity)
--- website/models.py
+++ website/models.py
@@ -1,13 +1,28 @@
 from django.db import models
 from django.forms import ModelForm, Textarea
-from django.contrib.auth.models import User, UserManager
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 
 
+###########  Extend user profile
+# Docs: http://stackoverflow.com/a/965883/705945
+#
 class UserProfile(models.Model):
     user = models.OneToOneField(User)
     bio = models.CharField(max_length=2000, blank=True)
-
     # company = models.ForeignKey('Company')
+
+    def __str__(self):
+        return "%s's profile" % self.user
+
+
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        profile, created = UserProfile.objects.get_or_create(user=instance)
+
+
+post_save.connect(create_user_profile, sender=User)
+###########  Extend user profile
 
 
 class Project(models.Model):
@@ -30,6 +45,7 @@ class ProjectForm(ModelForm):
             'description': Textarea(attrs={'cols': 80, 'rows': 10}),
         }
 
+
 class Opportunity(models.Model):
     project = models.ForeignKey(Project)
     name = models.CharField(max_length=100, blank=True)
@@ -42,15 +58,16 @@ class Opportunity(models.Model):
     # time estimate - TODO: do we do this in days?
     # deliverable - TODO: separate free-form text field
     # followup gift to volunteer - TODO: separate free-form text field
-    
+
     def __unicode__(self):
         return self.name
+
 
 class OpportunityForm(ModelForm):
     class Meta:
         model = Opportunity
         fields = ('name', 'description',)
-        
+
         widgets = {
             'description': Textarea(attrs={'cols': 80, 'rows': 10}),
         }
