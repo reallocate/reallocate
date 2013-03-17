bdae38bc63c1168ef76ff36471d582fc72a9dece
David Dehghan <ddehghan@gmail.com>
2013-03-16 22:57:38 -0700

    added opportunity list

--- website/views.py
+++ website/views.py
@@ -7,7 +7,6 @@ from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import User
 from django.views.decorators.csrf import csrf_exempt, csrf_protect
 
-
 from website.lib.ses_email import send_email
 from models import UserProfile
 from models import Project
@@ -15,6 +14,7 @@ from models import Opportunity
 from models import Update
 
 from website.models import ProjectForm, OpportunityForm, Project, Opportunity
+
 
 @login_required
 def private(request):
@@ -28,34 +28,50 @@ def login_page(request):
         'a': 'a',
     }, context_instance=RequestContext(request))
 
-# landing_instance.ip_address = request.META['REMOTE_ADDR']
 
 def homepage(request):
     opportunities = Opportunity.objects.all()[:6]
-    return render_to_response('homepage.html', { 'opportunities': opportunities }, context_instance=RequestContext(request))
+    projects = Opportunity.objects.all()[:6]
+
+    return render_to_response('homepage.html',
+                              {'opportunities': opportunities,
+                               'projects': projects},
+                              context_instance=RequestContext(request))
+
+
+def opportunity_list(request):
+    opportunities = Opportunity.objects.all()[:6]
+    projects = Opportunity.objects.all()[:6]
+
+    return render_to_response('opportunity_list.html',
+                              {'opportunities': opportunities,
+                               'projects': projects},
+                              context_instance=RequestContext(request))
+
 
 def test_project(request):
-
     return render_to_response('project.html', {}, context_instance=RequestContext(request))
+
 
 @csrf_exempt
 def signup(request):
     if request.method == 'GET':
         return render_to_response('signup.html')
-    
+
     email = request.POST.get('email')
     password = request.POST.get('password')
 
     # Create a new user and persist it to the database.
     user = User.objects.create_user(username=email, email=email, password=password)
     user.save()
-  
+
     # Append the newly-created user with a company.
     user_profile = UserProfile()
     user_profile.user = user
     user_profile.save()
-    
+
     return HttpResponseRedirect('/login/')
+
 
 @csrf_exempt
 def login_user(request):
@@ -74,25 +90,27 @@ def login_user(request):
                 state = "Your account is not active, please contact the site admin."
         else:
             state = "Your username and/or password were incorrect."
-    return render_to_response('auth.html',{'state':state, 'username': username})
+    return render_to_response('auth.html', {'state': state, 'username': username})
+
 
 def view_project(request, *args):
-
     project = get_object_or_404(Project, pk=args[0])
-    opportunities= Opportunity.objects.filter(project=project)
+    opportunities = Opportunity.objects.filter(project=project)
     updates = Update.objects.filter(project=project)
     return render_to_response('project.html', {
-                            "project": project,
-                            "updates": updates,
-                            "opportunities":opportunities,
-        }, context_instance=RequestContext(request))
+        "project": project,
+        "updates": updates,
+        "opportunities": opportunities,
+    }, context_instance=RequestContext(request))
+
 
 def view_opportunity(request, *args):
     opp = get_object_or_404(Opportunity, pk=args[0])
     return render_to_response('opportunity.html', {
-                            "opportunity": opp,
-                            "project": opp.project
-        }, context_instance=RequestContext(request))
+        "opportunity": opp,
+        "project": opp.project
+    }, context_instance=RequestContext(request))
+
 
 def add_project(request):
     # Show the sign page and collect emails
@@ -121,7 +139,7 @@ def add_opportunity(request, oid=1):
     # Create new Opportunity
     parent_project = get_object_or_404(Project, pk=oid)
     show_form = True
-    
+
     if request.method == "POST":
         myform = OpportunityForm(request.POST)
         new_instance = myform.save(commit=False)
@@ -131,7 +149,7 @@ def add_opportunity(request, oid=1):
             show_form = False
         else:
             return HttpResponse("error")
- 
+
     myform = OpportunityForm()
     return render_to_response('add_opportunity.html', {
         "myform": myform,
