f5968cb9edfc484f071edc4dd34991e7cfa3f059
David Merritt <david@bbfdirect.com>
2013-04-04 17:54:19 -0700

    add ses email sending - make sure to update settings_local.py for secret key

--- website/base.py
+++ website/base.py
@@ -1,6 +1,7 @@
 from website.models import UserProfile
 from django.http import HttpResponse, HttpResponseRedirect
-from django.core.mail import send_mail
+from django.core.mail import EmailMessage
+from myproject.settings import FROM_EMAIL
 
 def get_current_userprofile(request):
     try:
@@ -19,5 +20,10 @@ def build_base_context(request):
     model['topmsg'] = request.GET.get('topmsg')
     return model
 
-#def notify_users(project):
+def send_email(recipients, body, subject, from_email=FROM_EMAIL, headers=None):
+    # https://docs.djangoproject.com/en/dev/topics/email/
+    if not isinstance(recipients, list):
+        recipients = recipients.split(",")
+    msg = EmailMessage(subject, body, from_email, recipients, headers=headers)
+    msg.send()
 
