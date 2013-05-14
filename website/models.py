7d8148c8c0b56b1901a4eed23f25a18239c1710b
David Merritt <david@bbfdirect.com>
2013-05-13 23:23:57 -0700

    unbreak /engage and add fixture data for engagnement

--- website/models.py
+++ website/models.py
@@ -133,5 +133,6 @@ class OpportunityEngagement(models.Model):
     user = models.ForeignKey(User)
     opportunity = models.ForeignKey(Opportunity)
     date_created = models.DateField(auto_now_add=True)
-    status = models.CharField(max_length=100, choices=STATUS_CHOICES) # this will be where the opp engagements can be approved
-    # response = models.CharField(max_length=2000) # response to the engagement
+     # this will be where the opp engagements can be approved
+    status = models.CharField(max_length=100, choices=STATUS_CHOICES, default=STATUS_CHOICES[0][0])
+    # response = models.CharField(max_length=2000, blank=True) # response to the engagement
