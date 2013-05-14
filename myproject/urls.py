8635caf4535a677fffa7bf2214b9ca6bf27e0f9d
David Merritt <david@bbfdirect.com>
2013-05-13 23:11:32 -0700

    re-arrange urls so opp/engage works - duplicate line

--- myproject/urls.py
+++ myproject/urls.py
@@ -24,10 +24,9 @@ urlpatterns = patterns('',
                        url(r'^project/(?P<pid>\d+)', 'website.views.view_project', name='view_project'),
 
                        url(r'^add_opportunity', 'website.views.add_opportunity', name='add_opportunity'),
-                       url(r'^opportunity/(?P<pid>\d+)', 'website.views.view_opportunity', name='view_opportunity'),
  
-                       url(r'^opportunity/(?P<pid>\d+)$', 'website.views.view_opportunity', name='view_opportunity'),
                        url(r'^opportunity/(?P<pid>\d+)/engage', 'website.views.engage', name='engage'),
+                       url(r'^opportunity/(?P<pid>\d+)$', 'website.views.view_opportunity', name='view_opportunity'),
 
                        url(r'^ajax/modify_project_relation', 'website.ajaxviews.modify_project_relation', name='modify_project_relation'),
                        url(r'^ajax/add-update', 'website.ajaxviews.add_update', name='add_update'),
