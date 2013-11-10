14c21cfa461e4db9ec60bd87dd19212a1684d957
Scott Leonard <scott@mixwe.com>
2013-11-09 18:34:44 -0800

    - stubbed out facebook profile pic fetch

--- website/base.py
+++ website/base.py
@@ -104,25 +104,18 @@ def get_user_avatar(backend, details, response, social_user, uid, user, *args, *
 
     url = None
 
-    logging.error(social_user)
-
     if backend.__class__ == FacebookBackend:
 
         url = "http://graph.facebook.com/%s/picture?type=large" % response['id']
- 
-    elif backend.__class__ == GoogleBackend:
-
-        url = response['picture']
-
-    logging.error(url)
 
     if url:
 
-        profile = user.get_profile()
-        avatar = urlopen(url).read()
-        fout = open(filepath, "wb") # filepath is where to save the image
-        fout.write(avatar)
-        fout.close()
-        profile.photo = url_to_image # depends on where you saved it
-        profile.save()
+        logging.error(url)
+        #profile = user.get_profile()
+        #avatar = urlopen(url).read()
+        #fout = open(filepath, "wb") # filepath is where to save the image
+        #fout.write(avatar)
+        #fout.close()
+        #profile.photo = url_to_image # depends on where you saved it
+        #profile.save()
 
