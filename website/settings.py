44ff82b3e01a499eaa7dfad207db85f9a826800b
Scott Leonard <scott@mixwe.com>
2014-03-07 16:21:18 -0600

    - commenting out addition static files dir until needed

--- website/settings.py
+++ website/settings.py
@@ -176,7 +176,7 @@ STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-    os.path.join(APP_NAME, 'static'),
+    #os.path.join(APP_NAME, 'static'),
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
