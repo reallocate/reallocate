229c5d86b3f4bc6293f5cd646cf58dda74bc34c6
Scott Leonard <scott@mixwe.com>
2014-03-12 20:36:41 -0600

    - manage.py have to be COBRAND aware too to properly collect statics

--- manage.py
+++ manage.py
@@ -3,8 +3,12 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "website.settings")
-    #os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cobrands.freespace.settings")
-    from django.core.management import execute_from_command_line
 
-    execute_from_command_line(sys.argv)
+	if os.environ.get('COBRAND'):
+		os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cobrands." + os.environ['COBRAND'] + ".settings")
+	else:
+		os.environ.setdefault("DJANGO_SETTINGS_MODULE", "website.settings")
+
+	from django.core.management import execute_from_command_line
+
+	execute_from_command_line(sys.argv)
