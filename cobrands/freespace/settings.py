da5a207d5b17400b6712493ec3428db91960ed24
Scott Leonard <scott@mixwe.com>
2014-03-12 20:14:24 -0600

    - forgot a dir

--- cobrands/freespace/settings.py
+++ cobrands/freespace/settings.py
@@ -7,7 +7,7 @@ APP_NAME = 'freespace'
 ROOT_URLCONF = 'cobrands.freespace.urls'
 INSTALLED_APPS = ('cobrands.freespace',) + INSTALLED_APPS
 MIDDLEWARE_CLASSES += ('cobrands.freespace.Middleware',)
-STATICFILES_DIRS += (os.path.join(PROJECT_ROOT, APP_NAME, 'static'),)
+STATICFILES_DIRS += (os.path.join(PROJECT_ROOT, 'cobrands', APP_NAME, 'static'),)
 
 BRAND = {
     'id': 'freespace',
